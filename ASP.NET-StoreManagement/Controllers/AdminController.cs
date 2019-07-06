7fcec5eaf5dabdf5e6a8d0bceac3e8d656732028
duythanh3101 <1612623@student.hcmus.edu.vn>
2019-07-07 05:34:08 +0700

    Load products in product manager

---
+++ ASP.NET-StoreManagement/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using ASP.NET_StoreManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_StoreManagement.Controllers
+{
+    public class AdminController : Controller
+    {
+        DBStoreManagmentEntities db = new DBStoreManagmentEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(db.Products);
+        }
+    }
+}
